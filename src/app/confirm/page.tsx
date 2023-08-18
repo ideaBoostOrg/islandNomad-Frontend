a84e87a728f0c5d6ca88be676e441d696e4fc848
Janith Madarasinghe <janithpm9991@gmail.com>
2023-08-18 07:39:41 +0530

    confirm page

---
+++ src/app/confirm/page.tsx
@@ -0,0 +1,10 @@
+import Container from '@/components/Container'
+import React from 'react'
+
+function ConfirmationPage() {
+    return (
+        <Container>ConfirmationPage</Container>
+    )
+}
+
+export default ConfirmationPage

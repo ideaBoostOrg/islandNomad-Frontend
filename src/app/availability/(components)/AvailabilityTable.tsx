82af4ea1a57ee25b5eee16dac0a8592d7bedd946
Janith Madarasinghe <janithpm9991@gmail.com>
2023-08-22 08:46:51 +0530

    Create AvailabilityTable.tsx

---
+++ src/app/availability/(components)/AvailabilityTable.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function AvailabilityTable() {
+    return (
+        <>
+            <div className="flex sticky top-0 z-[100] bg-white pt-6 border-b pb-4 justify-between item-start">
+                <h1 className="text-2xl font-bold">Availability</h1>
+                {/* <div className="flex gap-4">
+                    <h3 className="row-span-2 px-3 flex justify-center items-center text-white bg-primary-dark rounded-lg">8.0</h3>
+                    <div className="flex flex-col">
+                        <p className="font-semibold">{'Good'}</p>
+                        <p className="text-sm text-gray-700">{10} reviews</p>
+                    </div>
+                </div> */}
+            </div>
+        </>
+    )
+}
+
+export default AvailabilityTable
